886e3de9ad916242c965d3ab573e1ee46df83aaa
Ptolemy Barnes <ptolemybarnes@gmail.com>
2018-07-04 10:35:11 +0200

    adds not spec

--- day_06/spec/chance_spec.rb
+++ day_06/spec/chance_spec.rb
@@ -2,10 +2,10 @@ require 'chance'
 
 describe Chance do
   CERTAIN = Chance.new(1.0)
-	LIKELY = Chance.new(0.75)
-	EQUALLY_LIKELY = Chance.new(0.5)
-	UNLIKELY = Chance.new(0.25)
-	IMPOSSIBLE = Chance.new(0.0)
+  LIKELY = Chance.new(0.75)
+  EQUALLY_LIKELY = Chance.new(0.5)
+  UNLIKELY = Chance.new(0.25)
+  IMPOSSIBLE = Chance.new(0.0)
 
   describe 'tests equality' do
     it { expect(CERTAIN).to eq Chance.new(1.0) }
@@ -14,5 +14,14 @@ describe Chance do
     it { expect(UNLIKELY).to eq Chance.new(0.25) }
     it { expect(IMPOSSIBLE).to eq Chance.new(0.0) }
   end
+
+  describe 'tests not' do
+    it { expect(UNLIKELY).to eq LIKELY.not }
+    it { expect(LIKELY).to eq LIKELY.not.not }
+    it { expect(LIKELY).to eq !!LIKELY }
+    it { expect(CERTAIN).to eq IMPOSSIBLE.not }
+  end
 end
+
+
 
