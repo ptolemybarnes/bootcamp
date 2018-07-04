b306b4acbc1d241b9792a8b165da8d41fffee342
Ptolemy Barnes <ptolemybarnes@gmail.com>
2018-07-04 10:31:59 +0200

    Adds inheritance example

--- day_06/inheritance_example.rb
+++ day_06/inheritance_example.rb
@@ -1,16 +1,81 @@
-class Animal
-  def speak
-    "Hello!"
+
+class SingletonClass
+
+  def initialize
+    self.class.remove_method(:new)
+  end
+
+
+  private
+
+  def instance
+    self.instance
+  end
+
+  class << self
+    private 
+
+    def instance
+      if @instance.nil?
+        @instance = self.new
+      end
+    end
   end
 end
 
+SingletonClass.new
+SingletonClass.new
+
+class Animal
+  @@number_of_animals = 0
+
+  def initialize
+    @@number_of_animals += 1
+  end
+
+  def speak name
+    "Hello! My name is #{name}"
+  end
+
+  def swim
+    'Swim method in Animal'
+  end
+
+  class << self
+    def hi
+    end
+  end
+
+end
+
+module Swimmable
+
+  def swim
+    'I am swimming in the module Swimmable!'
+  end
+
+end
+
 class Dog < Animal
+  include Swimmable
 end
 
 class Cat < Animal
+  include Swimmable
+
+  def initialize
+    @something = 'hi'
+  end
+
+  def speak name
+    super + '. I am a cat.'
+  end
+
 end
 
-bob = Dog.new
 kitty = Cat.new
-puts bob.speak
-puts kitty.speak   
+dog = Dog.new
+
+puts kitty.speak 'kitty'
+puts kitty.swim
+puts dog.swim
